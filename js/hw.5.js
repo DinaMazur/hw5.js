244814957dde86e29f1bc57bc66c8317c6933e19
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-11-23 11:12:35 +0200

    Add files via upload

---
+++ js/hw.5.js
@@ -0,0 +1,98 @@
+const drinks = prompt("Виберіть напій (чай кава сік)");
+
+switch (drinks) {
+    case "Чай": 
+    alert("Ви вибрали чай");
+    break 
+    case "кава": 
+    alert("Ви вибрали каву");
+    break 
+    case "сік": 
+    alert("Ви вибрали сік");
+    break 
+}
+
+const weekDays = prompt("Виберіть день тиждень");
+
+switch (weekDays) {
+case "понеділок":
+ alert("нажаль це робочий день");
+break
+case "вівторок":
+    alert("нажаль це робочий день");
+   break
+   case "середа":
+ alert("нажаль це робочий день");
+break
+case "четверг":
+ alert("нажаль це робочий день");
+break
+case "п'ятниця":
+ alert("нажаль це робочий день");
+break
+case "субота":
+ alert("ура це вихідний");
+break
+case "неділя":
+ alert("ура це вихідний");
+break  
+}
+
+const month = prompt("Виберіть місяць року");
+
+switch (month) {
+case "січень":
+ alert("Це зима");
+break
+case "лютий":
+ alert("Це зима");
+break
+case "березень":
+ alert("Це весна");
+break
+case "квітень":
+ alert("Це весна");
+break
+case "травень":
+ alert("Це весна");
+break
+case "червень":
+ alert("Це літо");
+break
+case "липень":
+ alert("Це літо");
+break
+case "серпень":
+ alert("Це літо");
+break
+case "вересень":
+ alert("Це осінь");
+break
+case "жовтень":
+ alert("Це осінь");
+break
+case "листопад":
+ alert("Це осінь");
+break
+case "грудень":
+ alert("Це зима");
+break
+}
+
+const trafficLights = prompt("Введіть колір");
+
+switch (trafficLights) {
+case "червоний":
+alert("стоп");
+break
+case "зелений":
+alert("йди");
+break
+case "жовтий":
+alert("чекати");
+break
+}
+
+
+
+
